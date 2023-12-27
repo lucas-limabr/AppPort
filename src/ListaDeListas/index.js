a80f4fec731b0741f81c7878b32c190d3ef6dc10
Jonathas Quintão <jonathasquintaosilva2002@gmail.com>
2023-12-27 14:31:43 -0300

    Markdown implementado

--- src/ListaDeListas/index.js
+++ src/ListaDeListas/index.js
@@ -23,7 +23,7 @@ import "firebase/firestore";
 import Markdown from "react-native-markdown-display";
 
 export default function Questoes() {
-  const [value, setValue] =useState("first");
+  const [value, setValue] = useState("first");
   const [pergunta, setPergunta] = useState(null);
   const [respostaCorreta, setRespostaCorreta] = useState(null);
   const [id, setId] = useState(null);
@@ -32,12 +32,12 @@ export default function Questoes() {
   const [indice, setIndice] = useState(0);
   const [atualizarDados, setAtualizarDados] = useState(false);
   const route = useRoute();
-  const [questaoEstaNaLista, setQuestaoEstaNaLista] = useState(false)
+  const [questaoEstaNaLista, setQuestaoEstaNaLista] = useState(false);
 
   const navigation = useNavigation();
 
   const codigo = route.params.idLista;
-  const [idLista, setIdLista] = useState(null)
+  const [idLista, setIdLista] = useState(null);
 
   const db = getFirestore(FIREBASE_APP);
 
@@ -56,7 +56,6 @@ export default function Questoes() {
       const firstDocument = querySnapshot.docs[indice];
       const data = firstDocument.data();
       const questaoId = firstDocument.id;
-      
 
       return {
         id: questaoId,
@@ -71,18 +70,18 @@ export default function Questoes() {
 
   async function obterIdPorCodigo(codigo, colecao) {
     const colecaoRef = collection(db, colecao);
-  
+
     // Crie uma consulta para encontrar documentos com base no campo "codigo"
     const q = query(colecaoRef, where("codigo", "==", codigo));
-  
+
     try {
       // Execute a consulta e obtenha os resultados
       const querySnapshot = await getDocs(q);
-  
+
       // Se houver pelo menos um documento correspondente, retorne o ID do primeiro
       if (!querySnapshot.empty) {
         const primeiroDocumento = querySnapshot.docs[0];
-        
+
         return primeiroDocumento.id;
       } else {
         console.log("Nenhum documento encontrado com o código fornecido.");
@@ -92,41 +91,41 @@ export default function Questoes() {
       console.error("Erro ao obter ID por código:", error);
       return null;
     }
-  } 
+  }
 
   const selecionarQuestao = async (questaoId) => {
     try {
       const listaRef = doc(db, "listas", idLista);
-  
+
       // Obtém os dados atuais da lista
       const listaDoc = await getDoc(listaRef);
       const listaData = listaDoc.data();
-  
+
       // Verifica se a questão já está na lista
       const questaoRef = doc(db, "questoes", questaoId);
-  
+
       const questaoDoc = await getDoc(questaoRef);
-  
+
       if (questaoDoc.exists()) {
         const questaoIndex = listaData.questoes.findIndex(
           (ref) => ref.path === questaoRef.path
         );
-  
+
         if (questaoIndex !== -1) {
           // Se a questão já está na lista, remove ela
           const novaLista = [...listaData.questoes];
           novaLista.splice(questaoIndex, 1);
-  
+
           // Atualiza a lista no Firestore
           await updateDoc(listaRef, { questoes: novaLista });
         } else {
           // Se a questão não está na lista, adiciona ela
           const novaLista = [...listaData.questoes, questaoRef];
-  
+
           // Atualiza a lista no Firestore
           await updateDoc(listaRef, { questoes: novaLista });
         }
-  
+
         // Aguarde a resolução da Promise antes de definir o estado
         const estaNaLista = await verificarArray(questaoId);
         setQuestaoEstaNaLista(estaNaLista);
@@ -144,20 +143,19 @@ export default function Questoes() {
       // Crie referências para a lista e a questão
       const listaRef = doc(db, "listas", idLista); // Utilize idLista aqui
       const questaoRef = doc(db, "questoes", id);
-  
+
       // Obtenha os dados atuais da lista
       const listaDoc = await getDoc(listaRef);
-  
+
       if (listaDoc.exists()) {
         const listaData = listaDoc.data();
-  
+
         // Verifique se a questão está na lista
         const questaoEstaNaLista = listaData?.questoes?.some(
           (questao) =>
             questao?.path === questaoRef.path && questaoRef.path !== null
         );
-  
-  
+
         return questaoEstaNaLista;
       }
     } catch (error) {
@@ -165,8 +163,6 @@ export default function Questoes() {
       return false;
     }
   };
-  
-
 
   function continuar() {
     setIndice(indice + 1);
@@ -188,23 +184,23 @@ export default function Questoes() {
       setUrlImagem(result.urlImagem);
       setId(result.id);
     });
-  
+
     const verirficarEAtualizarEstado = async () => {
       const estaNaLista = await verificarArray(id);
       setQuestaoEstaNaLista(estaNaLista);
     };
-  
+
     const obterIdLista = async () => {
       const idListaObtido = await obterIdPorCodigo(codigo, "listas");
       setIdLista(idListaObtido);
     };
-  
+
     // Utilize uma função async dentro do useEffect
     const executarEfeitos = async () => {
       await verirficarEAtualizarEstado(); // Aguarde a verificação do estado
       await obterIdLista(); // Aguarde a obtenção do idLista
     };
-  
+
     executarEfeitos();
   }, [atualizarDados]);
 
@@ -234,7 +230,22 @@ export default function Questoes() {
               resizeMode="contain"
             />
           </View>
-          <Text style={styles.txtEnunciado}>{pergunta}</Text>
+          <Markdown
+            style={{
+              body: {
+                fontSize: 16,
+                color: "#fff",
+                top: 0,
+                width: "90%",
+                left: 5,
+                padding: 5,
+                textAlign: "left",
+                fontFamily: "Inder_400Regular",
+              },
+            }}
+          >
+            {pergunta}
+          </Markdown>
         </View>
 
         <View style={styles.containerResposta}>
@@ -246,7 +257,22 @@ export default function Questoes() {
                   : styles.alternativas,
               ]}
             >
-              <Text style={styles.txtEnunciado}>{resposta[0]}</Text>
+              <Markdown
+                style={{
+                  body: {
+                    fontSize: 16,
+                    color: "#fff",
+                    top: 0,
+                    width: "90%",
+                    left: 5,
+                    padding: 5,
+                    textAlign: "left",
+                    fontFamily: "Inder_400Regular",
+                  },
+                }}
+              >
+                {resposta[0]}
+              </Markdown>
             </TouchableOpacity>
             <TouchableOpacity
               style={[
@@ -255,7 +281,22 @@ export default function Questoes() {
                   : styles.alternativas,
               ]}
             >
-              <Text style={styles.txtEnunciado}>{resposta[1]}</Text>
+              <Markdown
+                style={{
+                  body: {
+                    fontSize: 16,
+                    color: "#fff",
+                    top: 0,
+                    width: "90%",
+                    left: 5,
+                    padding: 5,
+                    textAlign: "left",
+                    fontFamily: "Inder_400Regular",
+                  },
+                }}
+              >
+                {resposta[1]}
+              </Markdown>
             </TouchableOpacity>
             <TouchableOpacity
               style={[
@@ -264,7 +305,22 @@ export default function Questoes() {
                   : styles.alternativas,
               ]}
             >
-              <Text style={styles.txtEnunciado}>{resposta[2]}</Text>
+              <Markdown
+                style={{
+                  body: {
+                    fontSize: 16,
+                    color: "#fff",
+                    top: 0,
+                    width: "90%",
+                    left: 5,
+                    padding: 5,
+                    textAlign: "left",
+                    fontFamily: "Inder_400Regular",
+                  },
+                }}
+              >
+                {resposta[2]}
+              </Markdown>
             </TouchableOpacity>
             <TouchableOpacity
               style={[
@@ -273,7 +329,22 @@ export default function Questoes() {
                   : styles.alternativas,
               ]}
             >
-              <Text style={styles.txtEnunciado}>{resposta[3]}</Text>
+              <Markdown
+                style={{
+                  body: {
+                    fontSize: 16,
+                    color: "#fff",
+                    top: 0,
+                    width: "90%",
+                    left: 5,
+                    padding: 5,
+                    textAlign: "left",
+                    fontFamily: "Inder_400Regular",
+                  },
+                }}
+              >
+                {resposta[3]}
+              </Markdown>
             </TouchableOpacity>
           </ScrollView>
         </View>
